ca8bfa6b1c866be45349b630df28bf33f755eb66
Julie-Ha <69715947+Julie-Ha@users.noreply.github.com>
2020-08-25 11:51:44 +0200

    Ajout de l'apparition d'un ours

--- js/bear.js
+++ js/bear.js
@@ -5,14 +5,42 @@ class Bear {
 		this.x = 0;
 		this.y = 0;
 		this.sprite = 'img/bear.png';
+        this.spawned = false;
 	}
 	update() {
-
+        if (!this.spawned) {
+            this.playSpawnAnimation();
+        }
 	}
 	draw() {
 		let img = new Image();
 		img.src = this.sprite;
 		ctx.drawImage(img, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height);
 	}
+    // generateRandomHeightSpawn() {
+    //     min = machine.height / 2 + this.height / 6;
+    //     max = machine.height / 2 + this.height / 2;
+    //     return Math.floor(Math.random() * (max - min)) + min;
+    // }
+    playSpawnAnimation() {
+        if (this.y > machine.height / 2 + this.height / 4) {
+            this.y--;
+        } else {
+            this.spawned = true;
+        }
+    }
 }
 
+function generateRandomPosition(bear) {
+    min = machine.xWin + machine.widthWin / 2;
+    max = machine.xWin + machine.widthWin - bear.width;
+    bear.x = Math.floor(Math.random() * (max - min)) + min;
+    bear.y = machine.height / 2 + bear.height;
+}
+
+function handleBears() {
+    let bear = new Bear();
+    generateRandomPosition(bear);
+    
+    return bear;
+}
--- js/game.js
+++ js/game.js
@@ -1,9 +1,14 @@
 const machine = new Machine();
 const claw = new Claw();
 
+const bearsArray = [];
+bearsArray.push(handleBears());
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 	ctx.drawImage(background, 0, 0);
+
+    bearsArray.forEach(bear => bear.draw());
 
 	claw.draw();
 	machine.draw();
@@ -14,6 +19,8 @@ function draw() {
 function update() {
 
     claw.update();
+
+    bearsArray.forEach(bear => bear.update());
 
 	draw();
 	requestAnimationFrame(update);
